6733324452458210f6a2d7f1339fc7a4195a3aa2
Hongzhi.Liu <1160899400@qq.com>
2019-01-15 09:10:03 +0800

    Create ArrayTest.java

---
+++ src/algorithm/ArrayTest.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import intf.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Hongzhi Liu  liu13407134075@gmail.com
+ * @date 2019/1/14 17:35
+ */
+public class ArrayTest implements Test {
+
+    private int[] testIntArray = {2, 3, 8, 4, 5, 7};
+
+    @Override
+    public void runTest() {
+
+    }
+
+
+    /**
+     * 返回某个int数组的前n个最大值
+     *
+     * @param array
+     * @return
+     */
+    private int[] getMaxNArray(int[] array, final int n) {
+        List<Integer> list = new ArrayList<>();
+        //to do
+
+        int[] result = new int[n];
+        return result;
+    }
+}
